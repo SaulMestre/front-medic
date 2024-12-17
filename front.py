9947bb8031bdaa7d652c0cf20ef4fd371ce776e1
SaulMestre <saulmestre@msn.com>
2024-12-17 12:44:16 +0100

    Eliminamos funciones innecesarias

--- front.py
+++ front.py
@@ -6,19 +6,6 @@ def home():
 # Mostrar la imagen
     st.image("imagen_logo.png")
 
-def about():
-    st.title("Acerca de")
-    st.write("Esta es una aplicación sencilla creada con Streamlit.")
-    st.write("Usa el menú lateral para navegar entre páginas.")
-
-def contact():
-    st.title("Contacto")
-    st.write("¡Gracias por visitarnos! Aquí puedes dejarnos un mensaje.")
-    name = st.text_input("Tu nombre:")
-    message = st.text_area("Tu mensaje:")
-    if st.button("Enviar"):
-        st.success("¡Gracias por tu mensaje!")
-
 # Menú de navegación
 st.sidebar.title("Navegación")
 page = st.sidebar.radio("Ir a", ["Página Principal", "Prediction"])
